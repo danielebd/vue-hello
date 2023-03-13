1d575f684f56b11903499952ab3b2b366d0d35cb
danielebd <danielebalebalestro@gmail.com>
2023-03-13 16:49:04 +0100

    commenti

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+//richiamare una funzionalità di Vue
 const {createApp} = Vue
-
+//tramite elemento appena richiamato creare una base dati da manipolare
 createApp({
+    //sezione dati
     data() {
         return {
+            //elemento da inserire nell' HTML
             message: 'hello Vue!'
         }
     }
+//punto di inserimento dati .mount
 }).mount('#app')
